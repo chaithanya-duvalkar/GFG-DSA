eaa9530aba626aa2d17492bbb570d07d56198d77
Chaithanya S <chaithanya9ig@gmail.com>
2025-09-10 00:32:00 +0530

    added rotateArray pgm

--- Arrays/rotateArray.java
+++ Arrays/rotateArray.java
@@ -1,6 +1,6 @@
 
 public class rotateArray {
-    static void reverse(int[] arr,int left,int right)
+    static void reverseArray(int[] arr,int left,int right)
     {
         while(left<right)
         {
@@ -16,9 +16,9 @@ public class rotateArray {
         int n=arr.length;
         d=d%n;
 
-        reverse(arr,0,d-1);
-        reverse(arr,d,n-1);
-        reverse(arr,0,n-1);
+        reverseArray(arr,0,d-1);
+        reverseArray(arr,d,n-1);
+        reverseArray(arr,0,n-1);
     }
     
 }
