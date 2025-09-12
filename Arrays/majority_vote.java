3b3da7e8f8e468e90b547018e7d85aca2ebd46ab
Chaithanya S <chaithanya9ig@gmail.com>
2025-09-12 23:57:20 +0530

    added majority_vote pgm

--- Arrays/majority_vote.java
+++ Arrays/majority_vote.java
@@ -13,7 +13,52 @@
 // Input:  arr[] = [3, 2, 2, 4, 1, 4]
 // Output: []
 // Explanation: There is no majority element.
-
-public class majority_vote {
-    
+import java.util.*;
+class majority_vote {
+    public ArrayList<Integer> findMajority(int[] arr) {
+        // Code here
+        int n=arr.length;
+        int c1=0,c2=0;
+        Integer a=null,b=null;
+        
+        for(int num:arr)
+        {
+            if(a!=null&&a==num)
+            {
+                c1++;
+            }
+            else if(b!=null&&b==num)
+            {
+                c2++;
+            }
+            else if(c1==0)
+            {
+                a=num;
+                c1=1;
+            }
+            else if(c2==0)a
+            {
+                b=num;
+                c2=1;
+            }
+            else
+            {
+                c1--;
+                c2--;
+            }
+        }
+        c1=0;
+        c2=0;
+        for(int num:arr)
+        {
+            if(a!=null&&num==a) c1++;
+            else if(b!=null&&num==b) c2++;
+        }
+        ArrayList<Integer> result=new ArrayList<>();
+        if (c1>n/3) result.add(a);
+        if (c2>n/3) result.add(b);
+        
+        Collections.sort(result);
+        return result;
+    }
 }
