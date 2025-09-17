20a124e1fc771da6e9134cba2390b890d9999fac
Chaithanya S <chaithanya9ig@gmail.com>
2025-09-17 22:42:17 +0530

    added HIndex pgm

--- sorting/lowerBound.java
+++ sorting/lowerBound.java
@@ -38,3 +38,21 @@ public class lowerBound {
 
     }
 };
+
+// public class LowerBound {
+//     public int lowerBound(int[] arr, int target) {
+//         int low = 0, high = arr.length - 1;
+//         int ans = arr.length;
+
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             if (arr[mid] >= target) {
+//                 ans = mid;
+//                 high = mid - 1;
+//             } else {
+//                 low = mid + 1;
+//             }
+//         }
+//         return ans;
+//     }
+// }
