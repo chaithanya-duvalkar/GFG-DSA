053f68ba86e04b0096cca2550a51314761a4e258
Chaithanya S <chaithanya9ig@gmail.com>
2025-10-28 23:13:47 +0530

    findrepititive pgm added

---
+++ bitmanipulation/findrepetitive.java
@@ -0,0 +1,18 @@
+public class findrepetitive {
+
+    public int findDuplicate(int[] arr) {
+        // code here
+        int duplicate=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++)
+        {
+            duplicate=duplicate^i;
+        }
+        for(int i=0;i<n;i++)
+        {
+            duplicate=duplicate^arr[i];
+        }
+        return duplicate;
+    }
+}
+
